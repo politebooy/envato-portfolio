2d6ed28258fbf64ff0e1abe0757039c576cb4764
unknown <billahmukim@gmail.com>
2021-07-20 10:20:33 +0600

    commited

--- src/components/6-Clients/Client.js
+++ src/components/6-Clients/Client.js
@@ -4,20 +4,42 @@ import MyCard from '../MyCard'
 import styles from './client.module.css'
 import Image from 'next/image'
 
-const clients = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+// const clients = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+const projects = [
+    { title: 'Khaticlub', desc: 'Khaticlub is an e-commerce platform which will be capable of providing every kind of goods and products.' },
+    { title: 'Ridelife', desc: 'Its services include ride-hailing, food delivery, package delivery, couriers, freight transportation.' },
+    { title: 'Pdpdrive', desc: 'Pdp Drive is a file storage and synchronization service developed by  Snigdhochowa' },
+    { title: 'EEPB', desc: 'EEPB  is a E-commerce Association.we will Advic insight, profiles and guides for established and aspiring entrepreneurs worldwide' },
+    { title: 'Itlife', desc: 'Itlife provides businesses with professional advice regarding their IT needs & Support.' },
+]
 
 export default function Client() {
     return (
         <div className={styles.container}>
-            <h1 className="secTitle">MY CLIENTS</h1>
+            <h1 className="secTitle">MY PROJECT</h1>
+            <br />
+            <Typography variant="h4" align="center">SnigdhoChowa Group Of Company Ltd.</Typography>
+            <br />
             <Grid container>
-                {clients.map(client => (
+                {/* {clients.map(client => (
                     <Grid item md="2">
                         <MyCard>
-                            <Image src="/images/1.png" alt="Vercel Logo" width={100} height={50} />
                         </MyCard>
-                    </Grid>
-                ))}
+                        </Grid>
+                    ))} */}
+                <Grid container>
+                    {projects.map(project => {
+                        return <Grid item md="4">
+                            <MyCard>
+                                <Image src="/images/company1.jpg" alt="Vercel Logo" width={200} height={200} />
+                                <Typography variant="h4" align="center">{project.title}</Typography>
+                                <Typography variant="body1" align="center">
+                                    {project.desc}
+                                </Typography>
+                            </MyCard>
+                        </Grid>
+                    })}
+                </Grid>
             </Grid>
         </div>
     )
