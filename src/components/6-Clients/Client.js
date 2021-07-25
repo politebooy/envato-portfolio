7a5c7ae21c4643d434f67ed3f751d0e3bd7558bf
unknown <billahmukim@gmail.com>
2021-07-26 05:38:44 +0600

    commited at 26 july

--- src/components/6-Clients/Client.js
+++ src/components/6-Clients/Client.js
@@ -6,16 +6,16 @@ import Image from 'next/image'
 
 // const clients = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 const projects = [
-    { title: 'Notelife', src: '/images/Untitled-1.png', link: 'http://notelife.me/', desc: 'Itlife provides businesses with professional advice regarding their IT needs & Support. and others.' },
-    { title: 'Notecloud', src: '/images/Notecloud-1-Fb-Profile.jpg', link: 'https://notecloud.net/', desc: 'Khaticlub is an e-commerce platform which will be capable of providing every kind of goods and products.' },
-    { title: 'Noteflix', src: '/images/Noteflix-1.png', link: 'http://noteflix.net/', desc: 'Its services include ride-hailing, food delivery, package delivery, couriers, freight transportation.' },
+    { title: 'Notelife', src: '/images/Untitled-1.png', link: 'http://notelife.me/', desc: 'Notelife Social Networking Platform.With our New Feature ,user can wonder posts,photos.' },
+    { title: 'Notecloud', src: '/images/Notecloud-1-Fb-Profile.jpg', link: 'https://notecloud.net/', desc: 'NoteCloud allows performing all students to submit  Note to colleges from around the world online.' },
+    { title: 'Noteflix', src: '/images/Noteflix-1.png', link: 'http://noteflix.net/', desc: 'Noteflix Social Entertaintment Platform.With our New Feature ,user can see Movie.' },
 
-    { title: 'Itlife', src: '/images/company1.jpg', link: 'https://itlife.one/', desc: 'Pdp Drive is a file storage and synchronization service developed by  Snigdhochowa' },
-    { title: 'Khaticlub', src: '/images/Khaticlub.png', link: 'https://khaticlub.com/', desc: 'EEPB  is a E-commerce Association.we will Advic insight, profiles and guides for established' },
+    { title: 'Itlife', src: '/images/company1.jpg', link: 'https://itlife.one/', desc: 'Itlife provides businesses with professional advice regarding their IT needs and Support.' },
+    { title: 'Khaticlub', src: '/images/Khaticlub.png', link: 'https://khaticlub.com/', desc: 'Khaticlub is an e-commerce platform which will be capable of providing every kind of goods and products.' },
     { title: 'Ridelife', src: '/images/Ridelife-logo.jpg', link: 'http://ridelife.me/', desc: 'Its services include ride-hailing, food delivery, package delivery, couriers, freight transportation.' },
 
     { title: 'Pdpdrive', src: '/images/Pdpdrive1.jpg', link: 'http://pdpdrive.com/', desc: 'Pdp Drive is a file storage and synchronization service developed by  Snigdhochowa' },
-    { title: 'EEPB', src: '/images/EEPB-1.png', link: 'http://eepb.com.bd/', desc: 'EEPB  is a E-commerce Association.we will Advic insight, profiles and guides for established' },
+    { title: 'EEPB', src: '/images/EEPB-1.png', link: 'http://eepb.com.bd/', desc: 'EEPB  is a E-commerce Association.we will Advic insight, profiles and guides for established and aspiring entrepreneurs worldwide' },
     { title: 'Newsstore', src: '/images/nnnnnnn.png', link: 'https://newsstore.info/', desc: 'Its services include ride-hailing, food delivery, package delivery, couriers, freight transportation.' },
 ]
 
